91da1a22e45245c62764b5069034bf8be41b6075
Adnan Rakib <adnanrakib064@gmail.com>
2025-03-01 02:47:54 +0530

    Initial commit with files from greenguard.bmu

---
+++ hacked3.0/update_files_v2.py
@@ -0,0 +1,20 @@
+# created by Mushfiq
+import os
+import time
+
+repo_path = '.'
+comment = '# created by Mushfiq\n'
+
+for subdir, _, files in os.walk(repo_path):
+    for file in files:
+        file_path = os.path.join(subdir, file)
+        if file_path.endswith(".py") or file_path.endswith(".ts") or file_path.endswith(".css"):
+            with open(file_path, 'r+', encoding='utf-8') as f:
+                content = f.read()
+                f.seek(0, 0)
+                f.write(comment + content)
+            
+            # Update the file modification time
+            os.utime(file_path, (time.time(), time.time()))
+
+print("All files updated successfully.")
